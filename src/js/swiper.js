3fc5944a039f8bec6a1c535beb99158d5f455d46
KostiZin <pancostian@gmail.com>
2023-04-07 12:06:49 -0400

    Updates about slider
    I added more functions to the slider. It works perfectly on mobile and tablet. However, there is still an issue with the desktop version.

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -3,10 +3,43 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
-  loop: true,
+    freeMode: true,
+    
+  
+  
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
+    
   },
+
+  mousewheel: {
+    sensitivity: 1,
+   
+  },
+
+  breakpoints: {
+
+    375: {
+      slidesPerView: 1,
+     spaceBetween: 20,
+    },
+
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 18,
+      
+    },
+
+    1200: {
+      slidePerView: 4,
+      spaceBetween: 18,
+    },
+  }
+
+  
+  
+  
 });
