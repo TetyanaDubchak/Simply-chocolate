7a2999ebbdcaf2457b566c3b5a37410e289761ba
MaksymBora <maxboraod@gmail.com>
2023-04-08 18:26:41 +0200

    swiper-review.js

---
+++ src/js/swiper-review.js
@@ -0,0 +1,30 @@
+
+$('.slick').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+  arrows: false,
+  infinite: true,
+  autoplay: false,
+  autoplaySpeed: 3000,
+  responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 2,
+      },
+      },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+    {
+      breakpoint: 376,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
