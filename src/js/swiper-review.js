59ecb2b7af7f61b7ca7d251d855ec0a37ce6e7ff
MaksymBora <maxboraod@gmail.com>
2023-04-08 18:23:45 +0200

    add slick done

--- src/js/swiper-review.js
+++ src/js/swiper-review.js
@@ -1,6 +1,6 @@
 
 $('.slick').slick({
-  slidesToShow: 4,
+  slidesToShow: 3,
   slidesToScroll: 1,
   dots: true,
   arrows: false,
@@ -13,6 +13,12 @@ $('.slick').slick({
       settings: {
         slidesToShow: 2,
       },
+      },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+      },
     },
     {
       breakpoint: 376,
@@ -23,28 +29,3 @@ $('.slick').slick({
   ],
 });
 
-var scroller = $('.slick-modal').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  dots: false,
-  arrows: false,
-  infinite: true,
-  autoplay: true,
-  autoplaySpeed: 3000,
-  responsive: [
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 400,
-      settings: {
-        slidesToShow: 1,
-        dots: true,
-      },
-    },
-  ],
-});
